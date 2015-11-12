0759b5ac8eeb954f822ff491fa5ad6bc46460345
Michael Adelson <mike.adelson314@gmail.com>
2015-11-12 05:51:59 -0500

    Made syntax rewrite test actually validate

--- InlineNuGet.Tests/SyntaxRewriterTest.cs
+++ InlineNuGet.Tests/SyntaxRewriterTest.cs
@@ -36,7 +36,22 @@ namespace Foo.Tests {
             var rewriter = new SyntaxRewriter("LIB", "Foo");
             var rewritten = rewriter.Visit(syntax);
 
-            this.output.WriteLine(Microsoft.CodeAnalysis.Formatting.Formatter.Format(rewritten, new AdhocWorkspace()).ToFullString());
+            var formatted = Microsoft.CodeAnalysis.Formatting.Formatter.Format(rewritten, new AdhocWorkspace()).ToFullString();
+            this.output.WriteLine(formatted);
+
+            var preprocessorSymbols = new[] { "LIB_PUBLIC", "LIB_DISABLE_EXTENSIONS", "LIB_USE_LOCAL_NAMESPACE" };
+            var permutations = (long)Math.Pow(2, preprocessorSymbols.Length);
+            for (var flags = 0; flags <= permutations; ++flags)
+            {
+                var symbols = preprocessorSymbols.Where((s, index) => (flags & (1L << index)) != 0);
+                var parsed = SyntaxFactory.ParseCompilationUnit(formatted.Replace("$rootnamespace$", "My.Root.Namespace"), options: new CSharpParseOptions(preprocessorSymbols: symbols));
+                if (parsed.ContainsDiagnostics)
+                {
+                    this.output.WriteLine($"{flags}: {string.Join(", ", symbols)}");
+                    this.output.WriteLine(string.Join(Environment.NewLine, parsed.GetDiagnostics()));
+                }
+                Assert.Equal(actual: parsed.ContainsDiagnostics, expected: false);
+            }
         }
     }
 }
