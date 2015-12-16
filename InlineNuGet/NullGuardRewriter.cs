72ccfa021ad871330dbfdfad7f64848203352899
Michael Adelson <mike.adelson314@gmail.com>
2015-12-16 07:01:16 -0500

    Some more prep for using Fody

---
+++ InlineNuGet/NullGuardRewriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medallion.Tools
+{
+    class NullGuardRewriter
+    {
+        // remove usings
+        // remove attributes
+        // add checks
+    }
+}
