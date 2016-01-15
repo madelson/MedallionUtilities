8510753b3dd81a148a84d42e32aaaeb70badc702
Michael Adelson <mike.adelson314@gmail.com>
2016-01-14 21:37:12 -0500

    commented out more base collections

--- MedallionCollections/CollectionBase.cs
+++ MedallionCollections/CollectionBase.cs
@@ -1,66 +1,66 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace Medallion.Collections
-{
-    // TODO
-    // CB, ROCB, DB, RODB
+//namespace Medallion.Collections
+//{
+//    // TODO
+//    // CB, ROCB, DB, RODB
 
-    public abstract class CollectionBase<TElement> : IReadOnlyCollection<TElement>, ICollection
-    {
-        public abstract int Count { get; }
+//    public abstract class CollectionBase<TElement> : IReadOnlyCollection<TElement>, ICollection
+//    {
+//        public abstract int Count { get; }
 
-        bool ICollection.IsSynchronized { get { return false; } }
+//        bool ICollection.IsSynchronized { get { return false; } }
 
-        private object _syncRoot;
-        object ICollection.SyncRoot
-        {
-            get { return LazyInitializer.EnsureInitialized(ref this._syncRoot, () => new object()); }
-        }
+//        private object _syncRoot;
+//        object ICollection.SyncRoot
+//        {
+//            get { return LazyInitializer.EnsureInitialized(ref this._syncRoot, () => new object()); }
+//        }
 
-        void ICollection.CopyTo(Array array, int index)
-        {
-            Throw.IfNull(array, nameof(array));
-            Throw.If(array.Rank != 1, nameof(array) + " must be one-dimensional");
-            Throw.If(index < 0, nameof(index) + "must be non-negative");
-            Throw.If(index + this.Count > array.Length, nameof(array) + " is not long enough");
+//        void ICollection.CopyTo(Array array, int index)
+//        {
+//            Throw.IfNull(array, nameof(array));
+//            Throw.If(array.Rank != 1, nameof(array) + " must be one-dimensional");
+//            Throw.If(index < 0, nameof(index) + "must be non-negative");
+//            Throw.If(index + this.Count > array.Length, nameof(array) + " is not long enough");
 
-            var i = index;
-            foreach (var element in this)
-            {
-                array.SetValue(element, i++);
-            }
-        }
+//            var i = index;
+//            foreach (var element in this)
+//            {
+//                array.SetValue(element, i++);
+//            }
+//        }
 
-        IEnumerator IEnumerable.GetEnumerator() { return this.InternalGetEnumerator(); }
+//        IEnumerator IEnumerable.GetEnumerator() { return this.InternalGetEnumerator(); }
 
-        public IEnumerator<TElement> GetEnumerator() { return this.InternalGetEnumerator(); }
+//        public IEnumerator<TElement> GetEnumerator() { return this.InternalGetEnumerator(); }
 
-        protected abstract IEnumerator<TElement> InternalGetEnumerator();
+//        protected abstract IEnumerator<TElement> InternalGetEnumerator();
 
-        public abstract bool Contains(TElement item);
+//        public abstract bool Contains(TElement item);
 
-        internal static NotSupportedException ReadOnly([CallerMemberName] string member = null)
-        {
-            return new NotSupportedException($"{member} is not supported: the collection is read-only");
-        }
+//        internal static NotSupportedException ReadOnly([CallerMemberName] string member = null)
+//        {
+//            return new NotSupportedException($"{member} is not supported: the collection is read-only");
+//        }
 
-        internal static void CopyToHelper(IReadOnlyCollection<TElement> collection, TElement[] array, int arrayIndex)
-        {
-            Throw.IfNull(array, nameof(array));
-            Throw.If(arrayIndex + collection.Count > array.Length, nameof(array) + " is not long enough");
+//        internal static void CopyToHelper(IReadOnlyCollection<TElement> collection, TElement[] array, int arrayIndex)
+//        {
+//            Throw.IfNull(array, nameof(array));
+//            Throw.If(arrayIndex + collection.Count > array.Length, nameof(array) + " is not long enough");
 
-            var i = arrayIndex;
-            foreach (var element in collection)
-            {
-                array[i++] = element;
-            }
-        }
-    }
-}
+//            var i = arrayIndex;
+//            foreach (var element in collection)
+//            {
+//                array[i++] = element;
+//            }
+//        }
+//    }
+//}
--- MedallionCollections/DictionaryBase.cs
+++ MedallionCollections/DictionaryBase.cs
@@ -1,270 +1,270 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Medallion.Collections
-{
-    public abstract class DictionaryBase<TKey, TValue> : CollectionBase<KeyValuePair<TKey, TValue>>, IReadOnlyDictionary<TKey, TValue>, IDictionary
-    {
-        TValue IReadOnlyDictionary<TKey, TValue>.this[TKey key]
-        {
-            get { return this.GetValue(key); }
-        }
+//namespace Medallion.Collections
+//{
+//    public abstract class DictionaryBase<TKey, TValue> : CollectionBase<KeyValuePair<TKey, TValue>>, IReadOnlyDictionary<TKey, TValue>, IDictionary
+//    {
+//        TValue IReadOnlyDictionary<TKey, TValue>.this[TKey key]
+//        {
+//            get { return this.GetValue(key); }
+//        }
 
-        protected TValue GetValue(TKey key)
-        {
-            TValue value;
-            if (!this.TryGetValue(key, out value))
-            {
-                throw new KeyNotFoundException();
-            }
-            return value;
-        }
+//        protected TValue GetValue(TKey key)
+//        {
+//            TValue value;
+//            if (!this.TryGetValue(key, out value))
+//            {
+//                throw new KeyNotFoundException();
+//            }
+//            return value;
+//        }
 
-        private static byte canKeyTypeBeNullCache;
+//        private static byte canKeyTypeBeNullCache;
 
-        private static bool CanKeyTypeBeNull()
-        {
-            const byte CanBeNull = 1, CannotBeNull = 2;
-            var cachedValue = canKeyTypeBeNullCache;
-            if (cachedValue != 0)
-            {
-                return cachedValue == CanBeNull;
-            }
+//        private static bool CanKeyTypeBeNull()
+//        {
+//            const byte CanBeNull = 1, CannotBeNull = 2;
+//            var cachedValue = canKeyTypeBeNullCache;
+//            if (cachedValue != 0)
+//            {
+//                return cachedValue == CanBeNull;
+//            }
 
-            var result = !typeof(TKey).IsValueType || Nullable.GetUnderlyingType(typeof(TKey)) != null;
-            canKeyTypeBeNullCache = result ? CanBeNull : CannotBeNull;
-            return result;
-        }
+//            var result = !typeof(TKey).IsValueType || Nullable.GetUnderlyingType(typeof(TKey)) != null;
+//            canKeyTypeBeNullCache = result ? CanBeNull : CannotBeNull;
+//            return result;
+//        }
 
-        protected abstract void Add(TKey key, TValue value, bool throwIfPresent);
+//        protected abstract void Add(TKey key, TValue value, bool throwIfPresent);
 
-        object IDictionary.this[object key]
-        {
-            get
-            {
-                TValue value;
-                if (((key == null && CanKeyTypeBeNull()) || (key is TKey))
-                    && this.TryGetValue((TKey)key, out value))
-                {
-                    return value;
-                }
+//        object IDictionary.this[object key]
+//        {
+//            get
+//            {
+//                TValue value;
+//                if (((key == null && CanKeyTypeBeNull()) || (key is TKey))
+//                    && this.TryGetValue((TKey)key, out value))
+//                {
+//                    return value;
+//                }
 
-                return null;
-            }
-            set
-            {
-                this.Add(key, value, throwIfPresent: false);
-            }
-        }
+//                return null;
+//            }
+//            set
+//            {
+//                this.Add(key, value, throwIfPresent: false);
+//            }
+//        }
 
-        private void Add(object key, object value, bool throwIfPresent)
-        {
-            // note: this try/catch approach is based on Dictionary<,>
+//        private void Add(object key, object value, bool throwIfPresent)
+//        {
+//            // note: this try/catch approach is based on Dictionary<,>
 
-            TKey typedKey;
-            try
-            {
-                typedKey = (TKey)key;
-            }
-            catch (InvalidCastException ex)
-            {
-                throw new ArgumentException($"{nameof(key)} must be of type {typeof(TKey)}", ex);
-            }
+//            TKey typedKey;
+//            try
+//            {
+//                typedKey = (TKey)key;
+//            }
+//            catch (InvalidCastException ex)
+//            {
+//                throw new ArgumentException($"{nameof(key)} must be of type {typeof(TKey)}", ex);
+//            }
 
-            TValue typedValue;
-            try
-            {
-                typedValue = (TValue)value;
-            }
-            catch (InvalidCastException ex)
-            {
-                throw new ArgumentException($"{nameof(value)} must be of type {typeof(TValue)}", ex);
-            }
+//            TValue typedValue;
+//            try
+//            {
+//                typedValue = (TValue)value;
+//            }
+//            catch (InvalidCastException ex)
+//            {
+//                throw new ArgumentException($"{nameof(value)} must be of type {typeof(TValue)}", ex);
+//            }
 
-            this.Add(typedKey, typedValue, throwIfPresent);
-        }
+//            this.Add(typedKey, typedValue, throwIfPresent);
+//        }
 
-        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys { get { return this.Keys; } }
+//        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys { get { return this.Keys; } }
 
-        private sealed class KeyCollection : ReadOnlyCollectionBase<TKey>
-        {
-            private readonly DictionaryBase<TKey, TValue> dictionary;
+//        private sealed class KeyCollection : ReadOnlyCollectionBase<TKey>
+//        {
+//            private readonly DictionaryBase<TKey, TValue> dictionary;
 
-            public KeyCollection(DictionaryBase<TKey, TValue> dictionary)
-            {
-                this.dictionary = dictionary;
-            }
+//            public KeyCollection(DictionaryBase<TKey, TValue> dictionary)
+//            {
+//                this.dictionary = dictionary;
+//            }
 
-            public override int Count { get { return this.dictionary.Count; } }
+//            public override int Count { get { return this.dictionary.Count; } }
 
-            public override bool Contains(TKey item)
-            {
-                return this.dictionary.ContainsKey(item);
-            }
+//            public override bool Contains(TKey item)
+//            {
+//                return this.dictionary.ContainsKey(item);
+//            }
 
-            protected override IEnumerator<TKey> InternalGetEnumerator()
-            {
-                return this.dictionary.Select(kvp => kvp.Key).GetEnumerator();
-            }
-        }
+//            protected override IEnumerator<TKey> InternalGetEnumerator()
+//            {
+//                return this.dictionary.Select(kvp => kvp.Key).GetEnumerator();
+//            }
+//        }
 
-        private KeyCollection keys;
+//        private KeyCollection keys;
 
-        public ReadOnlyCollectionBase<TKey> Keys
-        {
-            get { return this.keys ?? (this.keys = new KeyCollection(this)); }
-        }
+//        public ReadOnlyCollectionBase<TKey> Keys
+//        {
+//            get { return this.keys ?? (this.keys = new KeyCollection(this)); }
+//        }
 
-        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values { get { return this.Values; } }
+//        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values { get { return this.Values; } }
 
-        private sealed class ValueCollection : ReadOnlyCollectionBase<TValue>
-        {
-            private DictionaryBase<TKey, TValue> dictionary;
+//        private sealed class ValueCollection : ReadOnlyCollectionBase<TValue>
+//        {
+//            private DictionaryBase<TKey, TValue> dictionary;
 
-            public ValueCollection(DictionaryBase<TKey, TValue> dictionary)
-            {
-                this.dictionary = dictionary;
-            }
+//            public ValueCollection(DictionaryBase<TKey, TValue> dictionary)
+//            {
+//                this.dictionary = dictionary;
+//            }
 
-            public override int Count { get { return this.dictionary.Count; } }
+//            public override int Count { get { return this.dictionary.Count; } }
 
-            public override bool Contains(TValue item)
-            {
-                return Enumerable.Contains(this, item);
-            }
+//            public override bool Contains(TValue item)
+//            {
+//                return Enumerable.Contains(this, item);
+//            }
 
-            protected override IEnumerator<TValue> InternalGetEnumerator()
-            {
-                return this.dictionary.Select(kvp => kvp.Value).GetEnumerator();
-            }
-        }
+//            protected override IEnumerator<TValue> InternalGetEnumerator()
+//            {
+//                return this.dictionary.Select(kvp => kvp.Value).GetEnumerator();
+//            }
+//        }
 
-        private ValueCollection values;
+//        private ValueCollection values;
 
-        protected ReadOnlyCollectionBase<TValue> Values
-        {
-            get { return this.values ?? (this.values = new ValueCollection(this)); }
-        }
+//        protected ReadOnlyCollectionBase<TValue> Values
+//        {
+//            get { return this.values ?? (this.values = new ValueCollection(this)); }
+//        }
 
-        bool IDictionary.IsFixedSize { get { return this.IsReadOnly; } }
+//        bool IDictionary.IsFixedSize { get { return this.IsReadOnly; } }
 
-        bool IDictionary.IsReadOnly { get { return this.IsReadOnly; } }
+//        bool IDictionary.IsReadOnly { get { return this.IsReadOnly; } }
 
-        protected bool IsReadOnly { get; }
+//        protected bool IsReadOnly { get; }
 
-        ICollection IDictionary.Keys { get { return this.Values; } }
+//        ICollection IDictionary.Keys { get { return this.Values; } }
 
-        ICollection IDictionary.Values { get { return this.Values; } }
+//        ICollection IDictionary.Values { get { return this.Values; } }
 
-        public virtual bool ContainsKey(TKey key)
-        {
-            TValue ignored;
-            return this.TryGetValue(key, out ignored);
-        }
+//        public virtual bool ContainsKey(TKey key)
+//        {
+//            TValue ignored;
+//            return this.TryGetValue(key, out ignored);
+//        }
 
-        public abstract bool TryGetValue(TKey key, out TValue value);
+//        public abstract bool TryGetValue(TKey key, out TValue value);
 
-        void IDictionary.Add(object key, object value)
-        {
-            this.Add(key, value, throwIfPresent: true);
-        }
+//        void IDictionary.Add(object key, object value)
+//        {
+//            this.Add(key, value, throwIfPresent: true);
+//        }
 
-        void IDictionary.Clear()
-        {
-            this.InternalClear();
-        }
+//        void IDictionary.Clear()
+//        {
+//            this.InternalClear();
+//        }
 
-        protected abstract void InternalClear();
+//        protected abstract void InternalClear();
 
-        bool IDictionary.Contains(object key)
-        {
-            return ((key == null && CanKeyTypeBeNull()) || key is TKey)
-                && this.ContainsKey((TKey)key);
-        }
+//        bool IDictionary.Contains(object key)
+//        {
+//            return ((key == null && CanKeyTypeBeNull()) || key is TKey)
+//                && this.ContainsKey((TKey)key);
+//        }
 
-        IDictionaryEnumerator IDictionary.GetEnumerator()
-        {
-            return new DictionaryEnumerator(this.GetEnumerator());
-        }
+//        IDictionaryEnumerator IDictionary.GetEnumerator()
+//        {
+//            return new DictionaryEnumerator(this.GetEnumerator());
+//        }
 
-        private sealed class DictionaryEnumerator : IDictionaryEnumerator
-        {
-            private readonly IEnumerator<KeyValuePair<TKey, TValue>> enumerator;
-            private bool isEnumerating;
+//        private sealed class DictionaryEnumerator : IDictionaryEnumerator
+//        {
+//            private readonly IEnumerator<KeyValuePair<TKey, TValue>> enumerator;
+//            private bool isEnumerating;
 
-            public DictionaryEnumerator(IEnumerator<KeyValuePair<TKey, TValue>> enumerator)
-            {
-                this.enumerator = enumerator;
-            }
+//            public DictionaryEnumerator(IEnumerator<KeyValuePair<TKey, TValue>> enumerator)
+//            {
+//                this.enumerator = enumerator;
+//            }
 
-            object IEnumerator.Current
-            {
-                get
-                {
-                    this.CheckEnumerating();
-                    return this.enumerator.Current;
-                }
-            }
+//            object IEnumerator.Current
+//            {
+//                get
+//                {
+//                    this.CheckEnumerating();
+//                    return this.enumerator.Current;
+//                }
+//            }
 
-            DictionaryEntry IDictionaryEnumerator.Entry
-            {
-                get
-                {
-                    this.CheckEnumerating();
-                    return new DictionaryEntry(this.enumerator.Current.Key, this.enumerator.Current.Value);
-                }
-            }
+//            DictionaryEntry IDictionaryEnumerator.Entry
+//            {
+//                get
+//                {
+//                    this.CheckEnumerating();
+//                    return new DictionaryEntry(this.enumerator.Current.Key, this.enumerator.Current.Value);
+//                }
+//            }
 
-            object IDictionaryEnumerator.Key
-            {
-                get
-                {
-                    this.CheckEnumerating();
-                    return this.enumerator.Current.Key;
-                }
-            }
+//            object IDictionaryEnumerator.Key
+//            {
+//                get
+//                {
+//                    this.CheckEnumerating();
+//                    return this.enumerator.Current.Key;
+//                }
+//            }
 
-            object IDictionaryEnumerator.Value
-            {
-                get
-                {
-                    this.CheckEnumerating();
-                    return this.enumerator.Current.Value;
-                }
-            }
+//            object IDictionaryEnumerator.Value
+//            {
+//                get
+//                {
+//                    this.CheckEnumerating();
+//                    return this.enumerator.Current.Value;
+//                }
+//            }
 
-            bool IEnumerator.MoveNext()
-            {
-                return (this.isEnumerating = this.enumerator.MoveNext());
-            }
+//            bool IEnumerator.MoveNext()
+//            {
+//                return (this.isEnumerating = this.enumerator.MoveNext());
+//            }
 
-            void IEnumerator.Reset()
-            {
-                this.enumerator.Reset();
-                this.isEnumerating = false;
-            }
+//            void IEnumerator.Reset()
+//            {
+//                this.enumerator.Reset();
+//                this.isEnumerating = false;
+//            }
 
-            private void CheckEnumerating()
-            {
-                if (!this.isEnumerating)
-                {
-                    throw new InvalidOperationException("Enumeration has either not started or has already finished");
-                }
-            }
-        }
+//            private void CheckEnumerating()
+//            {
+//                if (!this.isEnumerating)
+//                {
+//                    throw new InvalidOperationException("Enumeration has either not started or has already finished");
+//                }
+//            }
+//        }
 
-        void IDictionary.Remove(object key)
-        {
-            if ((key == null && CanKeyTypeBeNull()) || key is TKey)
-            {
-                this.InternalRemove((TKey)key);
-            }
-        }
+//        void IDictionary.Remove(object key)
+//        {
+//            if ((key == null && CanKeyTypeBeNull()) || key is TKey)
+//            {
+//                this.InternalRemove((TKey)key);
+//            }
+//        }
 
-        protected abstract bool InternalRemove(TKey key);
-    }
-}
+//        protected abstract bool InternalRemove(TKey key);
+//    }
+//}
