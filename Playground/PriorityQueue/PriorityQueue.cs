fd15ef411eba460f57e518619889e523414c1d4d
Michael Adelson <mike.adelson314@gmail.com>
2016-07-24 13:19:46 -0400

    PriorityQueue

--- Playground/PriorityQueue/PriorityQueue.cs
+++ Playground/PriorityQueue/PriorityQueue.cs
@@ -9,23 +9,33 @@ using System.Threading.Tasks;
 
 namespace Medallion.Collections
 {
+    /// <summary>
+    /// Implements a min priority queue (https://en.wikipedia.org/wiki/Priority_queue) using a binary heap
+    /// </summary>
     [DebuggerTypeProxy(typeof(PriorityQueue<>.DebugView))]
     [DebuggerDisplay("Count = {Count}")]
     public sealed class PriorityQueue<T> : ICollection<T>, IReadOnlyCollection<T>
     {
+        // re-usable empty array instance
         private static readonly T[] EmptyArray = Enumerable.Empty<T>() as T[] ?? new T[0];
-
-        private readonly IComparer<T> comparer;
+        
         private T[] heap;
         private int version;
 
         #region ---- Constructors ----
+        /// <summary>
+        /// Constructs an instance of <see cref="PriorityQueue{T}"/>, optionaly using a specified <see cref="IComparer{T}"/>
+        /// </summary>
         public PriorityQueue(IComparer<T> comparer = null)
         {
-            this.comparer = comparer ?? Comparer<T>.Default;
+            this.Comparer = comparer ?? Comparer<T>.Default;
             this.heap = EmptyArray;
         }
 
+        /// <summary>
+        /// Constructs an instance of <see cref="PriorityQueue{T}"/> specifying an initial internal capacity
+        /// as well as an optional <see cref="IComparer{T}"/>
+        /// </summary>
         public PriorityQueue(int initialCapacity, IComparer<T> comparer = null)
             : this(comparer)
         {
@@ -40,50 +50,27 @@ namespace Medallion.Collections
             }
         }
 
+        /// <summary>
+        /// Constructs an instance of <see cref="PriorityQueue{T}"/> initially populated with the
+        /// given <paramref name="items"/>. Optionally uses the given <see cref="IComparer{T}"/>
+        /// </summary>
         public PriorityQueue(IEnumerable<T> items, IComparer<T> comparer = null)
             : this(comparer)
         {
-            if (items == null) { throw new ArgumentNullException(nameof(items)); }
-
-            ICollection<T> itemsCollection;
-            IReadOnlyCollection<T> readOnlyItemsCollection; 
-            if ((itemsCollection = items as ICollection<T>) != null)
-            {
-                if (itemsCollection.Count > 0)
-                {
-                    this.heap = new T[itemsCollection.Count];
-                    itemsCollection.CopyTo(this.heap, arrayIndex: 0);
-                    this.Count = itemsCollection.Count;
-                }
-            }
-            else if ((readOnlyItemsCollection = items as IReadOnlyCollection<T>) != null)
-            {
-                if (readOnlyItemsCollection.Count > 0)
-                {
-                    this.heap = new T[readOnlyItemsCollection.Count];
-                    foreach (var item in readOnlyItemsCollection)
-                    {
-                        this.heap[this.Count++] = item;
-                    }
-                }
-            }
-            else
-            {
-                foreach (var item in items)
-                {
-                    if (this.Count == this.heap.Length)
-                    {
-                        this.Expand();
-                    }
-                    this.heap[this.Count++] = item;
-                }
-            }
-
-            this.Heapify();
+            this.EnqueueRange(items);
         }
         #endregion
 
         #region ---- Priority Queue API ----
+        /// <summary>
+        /// The <see cref="IComparer{T}"/> instances which determines the sort order for the queue
+        /// </summary>
+        public IComparer<T> Comparer { get; }
+
+        /// <summary>
+        /// Adds <paramref name="item"/> to the <see cref="PriorityQueue{T}"/>. This operation is
+        /// amortized O(log(N)) in complexity
+        /// </summary>
         public void Enqueue(T item)
         {
             if (this.heap.Length == this.Count)
@@ -97,6 +84,12 @@ namespace Medallion.Collections
             ++this.version;
         }
 
+        /// <summary>
+        /// Removes and returns the minimum element of the <see cref="PriorityQueue{T}"/> as determined
+        /// by its <see cref="Comparer"/>. This operation is O(log(N)) in complexity.
+        /// 
+        /// Throws <see cref="InvalidOperationException"/> if the queue is empty
+        /// </summary>
         public T Dequeue()
         {
             if (this.Count == 0) { throw new InvalidOperationException("The priority queue is empty"); }
@@ -112,6 +105,12 @@ namespace Medallion.Collections
             return result;
         }
 
+        /// <summary>
+        /// Removes, but does not return the minimum element of the <see cref="PriorityQueue{T}"/> as determined
+        /// by its <see cref="Comparer"/>. This operation is O(1) in complexity.
+        /// 
+        /// Throws <see cref="InvalidOperationException"/> if the queue is empty
+        /// </summary>
         public T Peek()
         {
             if (this.Count == 0) { throw new InvalidOperationException("The priority queue is empty"); }
@@ -119,15 +118,45 @@ namespace Medallion.Collections
             return this.heap[0];
         }
 
+        /// <summary>
+        /// Adds all of the given <paramref name="items"/> to the <see cref="PriorityQueue{T}"/>. This operation
+        /// may run faster than repeated calls to <see cref="Enqueue(T)"/>
+        /// </summary>
         public void EnqueueRange(IEnumerable<T> items)
         {
             if (items == null) { throw new ArgumentNullException(nameof(items)); }
 
+            var initialCount = this.Count;
+            this.AppendItems(items);
 
+            if (this.Count == initialCount)
+            {
+                return; // no version bump
+            }
+
+            if (this.Count - initialCount > initialCount)
+            {
+                // Heapify() only operates on half the heap, so if we more than doubled the size
+                // then we should just heapify rather than swimming each new item
+                this.Heapify();
+            }
+            else
+            {
+                // otherwise, just swim each item as if we had called Enqueue() multiple times
+                for (var i = initialCount; i < this.Count; ++i)
+                {
+                    this.Swim(i, this.heap[i]);
+                }
+            }
+
+            ++this.version;
         }
         #endregion
         
         #region ---- Heap Maintenance Methods ----
+        /// <summary>
+        /// Grows the queue to hold at least one additional item
+        /// </summary>
         private void Expand()
         {
             var currentCapacity = (uint)this.heap.Length;
@@ -147,6 +176,10 @@ namespace Medallion.Collections
             Array.Resize(ref this.heap, (int)Math.Min(newCapacity, int.MaxValue));
         }
 
+        /// <summary>
+        /// Performs the heap "sink" operation starting at <paramref name="index"/>. <paramref name="item"/>
+        /// will be placed in its determined final position
+        /// </summary>
         private void Sink(int index, T item)
         {
             // from http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/util/PriorityQueue.java#653
@@ -159,13 +192,13 @@ namespace Medallion.Collections
                 var childIndex = (i << 1) + 1;
                 var childItem = this.heap[childIndex];
                 var rightChildIndex = childIndex + 1;
-                if (rightChildIndex < this.Count && this.comparer.Compare(childItem, this.heap[rightChildIndex]) > 0)
+                if (rightChildIndex < this.Count && this.Comparer.Compare(childItem, this.heap[rightChildIndex]) > 0)
                 {
                     childItem = this.heap[childIndex = rightChildIndex];
                 }
 
                 // if item is <= either child, stop
-                if (this.comparer.Compare(item, childItem) <= 0)
+                if (this.Comparer.Compare(item, childItem) <= 0)
                 {
                     break;
                 }
@@ -179,6 +212,10 @@ namespace Medallion.Collections
             this.heap[i] = item;
         }
 
+        /// <summary>
+        /// Performs the heap "swim" operation starting at <paramref name="index"/>. <paramref name="item"/>
+        /// will be placed in its determined final position
+        /// </summary>
         private void Swim(int index, T item)
         {
             // from http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/util/PriorityQueue.java#607
@@ -191,7 +228,7 @@ namespace Medallion.Collections
                 var parentItem = this.heap[parentIndex];
 
                 // if the parent is <= item, we're done
-                if (this.comparer.Compare(item, parentItem) >= 0)
+                if (this.Comparer.Compare(item, parentItem) >= 0)
                 {
                     break;
                 }
@@ -205,6 +242,9 @@ namespace Medallion.Collections
             this.heap[i] = item;
         }
 
+        /// <summary>
+        /// Builds a binary heap from an unstructured array
+        /// </summary>
         private void Heapify()
         {
             // from http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/util/PriorityQueue.java#673
@@ -215,6 +255,10 @@ namespace Medallion.Collections
             }
         }
 
+        /// <summary>
+        /// Appends the given <paramref name="items"/> to the heap without regard for maintaining
+        /// heap order
+        /// </summary>
         private void AppendItems(IEnumerable<T> items)
         {
             ICollection<T> itemsCollection;
@@ -254,6 +298,9 @@ namespace Medallion.Collections
         #endregion
 
         #region ---- Interface Members ----
+        /// <summary>
+        /// The number of items in the <see cref="PriorityQueue{T}"/>
+        /// </summary>
         public int Count { get; private set; }
 
         bool ICollection<T>.IsReadOnly => false;
@@ -261,6 +308,9 @@ namespace Medallion.Collections
         [EditorBrowsable(EditorBrowsableState.Never)]
         public void Add(T item) => this.Enqueue(item);
 
+        /// <summary>
+        /// Removes all items from the <see cref="PriorityQueue{T}"/>
+        /// </summary>
         public void Clear()
         {
             const int MaxRetainedCapacity = 1024;
@@ -278,6 +328,11 @@ namespace Medallion.Collections
             ++this.version;
         }
 
+        /// <summary>
+        /// Returns true if an only if the <see cref="PriorityQueue{T}"/> contains
+        /// the given <paramref name="item"/>. <see cref="Comparer"/> is used to determine
+        /// item equality
+        /// </summary>
         public bool Contains(T item) => this.Find(item).HasValue;
         
         void ICollection<T>.CopyTo(T[] array, int arrayIndex)
@@ -286,6 +341,10 @@ namespace Medallion.Collections
             Array.Copy(sourceArray: this.heap, sourceIndex: 0, destinationArray: array, destinationIndex: arrayIndex, length: this.Count);
         }
         
+        /// <summary>
+        /// Returns an <see cref="IEnumerator{T}"/> over the items in the <see cref="PriorityQueue{T}"/>.
+        /// Note that the items are not enumerated over in the order that they would be returned by <see cref="Dequeue"/>
+        /// </summary>
         public IEnumerator<T> GetEnumerator()
         {
             var version = this.version;
@@ -297,6 +356,10 @@ namespace Medallion.Collections
             }
         }
 
+        /// <summary>
+        /// Removes up to one instance of <paramref name="item"/> from the <see cref="PriorityQueue{T}"/> if
+        /// it exists, using <see cref="Comparer"/> to determine equality. Returns true only if the queue was modified
+        /// </summary>
         public bool Remove(T item)
         {
             var itemIndex = this.Find(item);
@@ -322,11 +385,15 @@ namespace Medallion.Collections
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
         #endregion
 
+        /// <summary>
+        /// Searches for <paramref name="item"/> in the <see cref="PriorityQueue{T}"/>. Returns either
+        /// an index where <paramref name="item"/> resides or null if no such index was found
+        /// </summary>
         private int? Find(T item) => this.Count > 0 ? this.FindHelper(item, startIndex: 0) : null;
 
         private int? FindHelper(T item, int startIndex)
         {
-            var cmp = this.comparer.Compare(item, this.heap[startIndex]);
+            var cmp = this.Comparer.Compare(item, this.heap[startIndex]);
             if (cmp == 0)
             {
                 // found it!
@@ -351,6 +418,9 @@ namespace Medallion.Collections
                  );             
         }
 
+        /// <summary>
+        /// Provides a cleaner view of the <see cref="PriorityQueue{T}"/> in the debugger
+        /// </summary>
         private sealed class DebugView
         {
             private readonly PriorityQueue<T> queue;
