7c6e1c15255a0223b74aa872fdfbab3831599c0a
Michael Adelson <mike.adelson314@gmail.com>
2016-07-29 06:23:55 -0400

    PriorityQueue nuspec

--- MedallionPriorityQueue/Properties/AssemblyInfo.cs
+++ MedallionPriorityQueue/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MedallionPriorityQueue")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A fast generic PriorityQueue data structure .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MedallionPriorityQueue")]
@@ -22,15 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f5808459-2ceb-4ce3-9973-936b7762211d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
