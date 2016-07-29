05f8e3ed5bec37a25fac46792c1d5ab126447893
Michael Adelson <mike.adelson314@gmail.com>
2016-07-29 06:25:51 -0400

    remove extraneous usings

--- MedallionPriorityQueue/PriorityQueue.cs
+++ MedallionPriorityQueue/PriorityQueue.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Medallion.Collections
 {
