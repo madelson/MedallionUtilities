47f163050019e62b028dd13708d549336b6d0f2a
Michael Adelson <mike.adelson314@gmail.com>
2017-06-01 20:29:38 -0400

    Add more cases and an actual parse to the LargeGrammarTest

--- Playground/BalancingTokenParse/Tests/LargeGrammarTest.cs
+++ Playground/BalancingTokenParse/Tests/LargeGrammarTest.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Medallion;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -33,6 +35,9 @@ namespace Playground.BalancingTokenParse.Tests
             var comma = new Token(",");
             var colon = new Token(":");
             var @return = new Token("return");
+            var goesTo = new Token("=>");
+            var var = new Token("var");
+            var assign = new Token("=");
 
             var start = new NonTerminal("Start");
             var stmt = new NonTerminal("Stmt");
@@ -43,6 +48,12 @@ namespace Playground.BalancingTokenParse.Tests
             var tupleMemberBinding = new NonTerminal("TupleMemberBinding");
             var tupleMemberBindingList = new NonTerminal("List<TupleMemberBinding>");
             var expBlock = new NonTerminal("ExpBlock");
+            var lambda = new NonTerminal("Lambda");
+            var lambdaParameters = new NonTerminal("LambdaArgs");
+            var lambdaParameterList = new NonTerminal("List<LambdaArg>");
+            var assignment = new NonTerminal("Assignment");
+            var call = new NonTerminal("Call");
+            var argList = new NonTerminal("List<Arg>");
 
             var rules = new Rule[]
             {
@@ -53,6 +64,7 @@ namespace Playground.BalancingTokenParse.Tests
 
                 new Rule(stmt, exp, semi),
                 new Rule(stmt, @return, exp, semi),
+                new Rule(stmt, assignment),
 
                 new Rule(exp, ident),
                 new Rule(exp, num),
@@ -61,8 +73,11 @@ namespace Playground.BalancingTokenParse.Tests
                 new Rule(exp, exp, plus, exp),
                 new Rule(exp, tuple),
                 new Rule(exp, expBlock),
+                new Rule(exp, lambda),
+                new Rule(exp, call),
 
                 new Rule(ident, id),
+                new Rule(ident, var),
 
                 new Rule(tuple, openParen, tupleMemberBindingList, closeParen),
 
@@ -73,10 +88,83 @@ namespace Playground.BalancingTokenParse.Tests
                 new Rule(tupleMemberBinding, ident, colon, exp),
 
                 new Rule(expBlock, openParen, stmt, stmtList, closeParen),
+            
+                new Rule(lambda, lambdaParameters, goesTo, exp),
+
+                new Rule(lambdaParameters, ident),
+                new Rule(lambdaParameters, openParen, lambdaParameterList, closeParen),
+
+                new Rule(lambdaParameterList),
+                new Rule(lambdaParameterList, ident, comma, lambdaParameterList),
+                new Rule(lambdaParameterList, ident),
+
+                new Rule(assignment, var, ident, assign, exp, semi),
+
+                new Rule(call, exp, openParen, argList, closeParen),
+
+                new Rule(argList),
+                new Rule(argList, exp, comma, argList),
+                new Rule(argList, exp),
             };
 
             var rewritten = LeftRecursionRewriter.Rewrite(rules, rightAssociativeRules: ImmutableHashSet<Rule>.Empty);
             var nodes = ParserBuilder.CreateParser(rewritten.Keys);
+            var parser = new ParserNodeParser(nodes, start, this.output.WriteLine);
+
+            var listener = new TreeListener(rewritten);
+            const string Code = @"
+                var a = 2;
+                var func = i => (var x = i + a; return x;);
+                var t = (z: a, y: func);
+                func(77);
+            ";
+            var tokens = Lex(Code, rules);
+            
+            Record.Exception(() => parser.Parse(tokens, listener))
+                .ShouldEqual(null);
+        }
+
+        private static List<Token> Lex(string code, IReadOnlyList<Rule> rules)
+        {
+            var allTokens = rules.SelectMany(r => r.Symbols)
+                .OfType<Token>()
+                .Distinct()
+                .ToDictionary(t => t.Name);
+
+            var parts = Regex.Split(code, @"\s+");
+            var result = new List<Token>();
+            foreach (var part in parts)
+            {
+                var remaining = part;
+                while (remaining.Length > 0)
+                {
+                    var identMatch = Regex.Match(remaining, @"^[^\W\d_]\w*");
+                    if (identMatch.Success)
+                    {
+                        result.Add(allTokens.TryGetValue(identMatch.Value, out var keyword) ? keyword : allTokens["ID"]);
+                        remaining = remaining.Substring(startIndex: identMatch.Length);
+                    }
+                    else
+                    {
+                        var numMatch = Regex.Match(remaining, @"^\d+");
+                        if (numMatch.Success)
+                        {
+                            result.Add(allTokens["NUM"]);
+                            remaining = remaining.Substring(startIndex: numMatch.Length);
+                        }
+                        else
+                        {
+                            var token = allTokens.Values.Where(t => remaining.StartsWith(t.Name))
+                                .OrderByDescending(t => t.Name.Length)
+                                .First();
+                            result.Add(token);
+                            remaining = remaining.Substring(startIndex: token.Name.Length);
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
